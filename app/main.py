7a2f014bcfda8465241596fed0369f9e3a3b1975
Burak Colak <burak.colak.yes@gmail.com>
2026-07-11 10:00:39 +0200

    chore: add endpoint skeleton

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,3 +11,40 @@ app = FastAPI(
     title="Order Rec API",
     version="1.0.0"
 )
+
+# Result Retrieval
+@app.get("/stores/{store_id}/recommendations")
+async def get_recommendations(
+    store_id: int,
+    date: str
+):
+    return {"store_id": store_id, "date": date, "recommendations": ["item1", "item2", "item3"]}
+
+# Data Ingestion
+@app.post("/load/items")
+async def upload_items(
+    file: UploadFile = File(...)
+):
+    content = await file.read()
+    return {"message": "Items uploaded successfully."}
+
+@app.post("/load/inventory")
+async def upload_inventory(
+    file: UploadFile = File(...)
+):
+    content = await file.read()
+    return {"message": "Inventory uploaded successfully."}
+
+@app.post("/load/orderable_items")
+async def upload_orderable_items(
+    file: UploadFile = File(...)
+):
+    content = await file.read()
+    return {"message": "Orderable items uploaded successfully."}
+
+@app.post("/load/recommendations")
+async def upload_recommendations(
+    file: UploadFile = File(...)
+):
+    content = await file.read()
+    return {"message": "Recommendations uploaded successfully."}
