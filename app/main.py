a45721d24a3cfabec87f0c06f0ddb53ccea7dc90
Burak Colak <burak.colak.yes@gmail.com>
2026-07-11 10:44:27 +0200

    feat: add response models

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
 import logging
+
+from datetime import date
 from fastapi import FastAPI, UploadFile, File
+
+
+from app.schemas import (
+    RecommendationResponse,
+    UploadResponse
+)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,38 +21,43 @@ app = FastAPI(
 )
 
 # Result Retrieval
-@app.get("/stores/{store_id}/recommendations")
+@app.get("/stores/{store_id}/recommendations", response_model=RecommendationResponse)
 async def get_recommendations(
-    store_id: int,
-    date: str
-):
-    return {"store_id": store_id, "date": date, "recommendations": ["item1", "item2", "item3"]}
+    store_id: str,
+    date: date
+)-> RecommendationResponse:
+    return RecommendationResponse(
+        store_id=store_id,
+        date=date,
+        count=0,
+        recommendations=[]
+    )
 
 # Data Ingestion
-@app.post("/load/items")
+@app.post("/load/items", response_model=UploadResponse)
 async def upload_items(
     file: UploadFile = File(...)
-):
+) -> UploadResponse:
     content = await file.read()
-    return {"message": "Items uploaded successfully."}
+    return UploadResponse(status="Success", message="Items uploaded successfully.", metadata=None)
 
-@app.post("/load/inventory")
+@app.post("/load/inventory", response_model=UploadResponse)
 async def upload_inventory(
     file: UploadFile = File(...)
-):
+) -> UploadResponse:
     content = await file.read()
-    return {"message": "Inventory uploaded successfully."}
+    return UploadResponse(status="Success", message="Inventory uploaded successfully.", metadata=None)
 
-@app.post("/load/orderable_items")
+@app.post("/load/orderable_items", response_model=UploadResponse)
 async def upload_orderable_items(
     file: UploadFile = File(...)
-):
+) -> UploadResponse:
     content = await file.read()
-    return {"message": "Orderable items uploaded successfully."}
+    return UploadResponse(status="Success", message="Orderable items uploaded successfully.", metadata=None)
 
-@app.post("/load/recommendations")
+@app.post("/load/recommendations", response_model=UploadResponse)
 async def upload_recommendations(
     file: UploadFile = File(...)
-):
+) -> UploadResponse:
     content = await file.read()
-    return {"message": "Recommendations uploaded successfully."}
+    return UploadResponse(status="Success", message="Recommendations uploaded successfully.", metadata=None)
--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,12 @@
 from typing import Literal, Optional
 from datetime import date
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 # lowercase, singular, no spaces, no special characters
 Category = Literal["fruits", "vegetables"]
 
-# Post Cleaned Schemas
+# Post Cleaned Schemas --------------
 class ItemRow(BaseModel):
     item_number: int
     name: str
@@ -29,7 +29,7 @@ class OrderableItemRow(BaseModel):
     purchase_price: float
     suggested_retail_price: float
     profit_margin: float
-    tags: Optional[str]
+    tags: Optional[str] = None
     category: Category
 
 class OrderRecommendationRow(BaseModel):
@@ -38,3 +38,81 @@ class OrderRecommendationRow(BaseModel):
     ordering_day: date
     delivery_day: date
     recommended_quantity: float
+
+
+# API Response Schemas ---------
+class Recommendation(BaseModel):
+    item_number: int
+    name: str
+    category: Category
+    current_inventory: float
+    recommended_quantity: float
+    delivery_day: date
+
+class Metadata(BaseModel):
+    file_name: str
+    rows_processed: int
+    rows_inserted: int
+    rows_skipped: int
+
+
+class RecommendationResponse(BaseModel):
+    """Response schema for the get /stores/{store_id}/recommendations endpoint"""
+
+    model_config = ConfigDict(
+        extra="forbid",
+        json_schema_extra={
+            "example": {
+                "store_id": "store_a",
+                "date": "2026-01-01",
+                "count": 2,
+                "recommendations": [
+                    {
+                        "item_number": 1023,
+                        "name": "Papaya",
+                        "category": "fruits",
+                        "current_inventory": 50,
+                        "recommended_quantity": 100,
+                        "delivery_day": "2026-01-02"
+                    },
+                    {
+                        "item_number": 1028,
+                        "name": "Cucumber",
+                        "category": "vegetables",
+                        "current_inventory": 30,
+                        "recommended_quantity": 80,
+                        "delivery_day": "2026-01-02"
+                    }
+                ]
+            }
+        }
+    )
+
+    store_id: str
+    date: date
+    count: int
+    recommendations: list[Recommendation]
+
+
+class UploadResponse(BaseModel):
+    """Response schema for the post /load/{data_type} endpoints"""
+
+    model_config = ConfigDict(
+        extra="forbid",
+        json_schema_extra={
+            "example": {
+                "status": "success",
+                "message": "Items data synced successfully.",
+                "metadata": {
+                    "file_name": "items.csv",
+                    "rows_processed": 100,
+                    "rows_inserted": 98,
+                    "rows_skipped": 2
+                }
+            }
+        }
+    )
+
+    status: str
+    message: str
+    metadata: Optional[Metadata] = None
